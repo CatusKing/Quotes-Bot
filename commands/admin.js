c21aee74d24bfbeabaf00f0348c1634ef5eb1bae
Thomas <chalkking17@gmail.com>
2021-11-14 18:06:57 -0500

    yep

---
+++ commands/admin.js
@@ -0,0 +1,63 @@
+const {botOwnerId, adminG, adminACh} = require('../general/config.json');
+
+module.exports = {
+    name: 'admin',
+    description: 'General admin commands',
+    options: [
+        {
+            type: 'SUB_COMMAND',
+            name: 'approve',
+            description: 'The command to approve quotes',
+            options: [
+                {
+                    type: 'INTEGER',
+                    name: 'quote-id',
+                    description: 'The id of the quote you want to approve',
+                    required: true
+                },
+                {
+                    type: 'BOOLEAN',
+                    name: 'status',
+                    description: 'Approve or Deny',
+                    required: true
+                }
+            ]
+        },
+        {
+            type: 'SUB_COMMAND',
+            name: 'ban',
+            description: 'Ban a user from using the bot',
+            options: [
+                {
+                    type: 'INTEGER',
+                    name: 'user',
+                    description: 'The user you want to ban',
+                    required: true
+                }
+            ]
+        }
+    ],
+    execute({client, interaction, db}) {
+        if (interaction.user.id !== botOwnerId) {
+            if (interaction.options.getSubcommand('approve')) {
+                const quote = db.get(`quotes.${interaction.options.getInteger('quote-id')}`);
+                if (!quote) interaction.reply('I couldn\'t find a quote with that id');
+                else {
+                    quote.approved = interaction.options.getBoolean('status');
+                    quote.denied = !interaction.options.getBoolean('status');
+                    db.set(`quotes.${interaction.options.getInteger('quote-id')}`, quote);
+                    interaction.reply(`Set Id: #${interaction.options.getInteger('quote-id')} to ${interaction.options.getBoolean('status')}`);
+                    if (quote.approved) {
+                        client.guilds.cache.get(adminG).channels.cache.get(adminACh).send(`New Quote by ${quote.author}!\n\`\`\`${quote.quote}\n\`\`\``).then(msg => {
+                            msg.crosspost().then();
+                        });
+                    }
+                }
+
+            } else if (interaction.options.getSubcommand('ban')) {
+                db.push('quotes.banned', interaction.options.getInteger('user'));
+                interaction.reply(`Banned user ${interaction.options.getInteger('user')}`);
+            }
+        } else interaction.reply(`Sorry you don't have permission to do this!`);
+    }
+}
--- commands/quote.js
+++ commands/quote.js
@@ -1,4 +1,4 @@
-const {adminCh} = require('../general/config.json');
+const {adminCh,botOwner} = require('../general/config.json');
 
 module.exports = {
     name: 'quote',
@@ -18,6 +18,8 @@ module.exports = {
         }
     ],
     execute({ client, interaction, db }) {
+        const banned = db.get('quotes.banned') || [];
+        if (banned.includes(interaction.user.id)) return interaction.reply(`Sorry you've been banned from submitting quotes on this bot\nContact ${botOwner} if you think this is a mistake`);
         const next = db.get('quotes.nextNum') || 0;
         const author = interaction.options.getString('author') || 'No Specified Author';
         db.set(`quotes.${next}`, {
@@ -29,6 +31,6 @@ module.exports = {
         });
         db.add(`quotes.nextNum`, 1);
         interaction.reply(`Added\`\`\`${interaction.options.getString('quote')}\n\`\`\`to the quote db\nAuthor: ${author}\nId: #${next}`);
-        client.channels.cache.get(adminCh).send(`New Quote\nId: #${next}\nAuthor: ${author}\nQuote:\`\`\`${interaction.options.getString('quote')}\n\`\`\``);
+        client.channels.cache.get(adminCh).send(`New Quote\nId: #${next}\nSubmitted by: ${interaction.user}\nAuthor: ${author}\nQuote:\`\`\`${interaction.options.getString('quote')}\n\`\`\``);
     }
 }
