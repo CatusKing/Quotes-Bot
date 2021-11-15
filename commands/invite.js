fec722bc97b7a88c4fd9e9e75755d16c3f3afdd9
Thomas <chalkking17@gmail.com>
2021-11-14 19:11:28 -0500

    yep

---
+++ commands/invite.js
@@ -0,0 +1,10 @@
+const {website,discord} = require('../general/config.json')
+
+module.exports = {
+    name: 'invite',
+    description: 'Send the guild invite and bot invite',
+    options: [],
+    execute({ interaction }) {
+        interaction.reply(`Invite me to your guild by going to ${website}\n\nOR\n\nAdd the quotes channel to your discord by joining [here](${discord}) and following the quotes channel`)
+    }
+}
